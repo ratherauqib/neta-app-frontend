2dcd83777223befad1bbad99a8105c4c93d25865
Mohd Auqib Rather <auqib.rather@venturepact.com>
2018-04-11 20:02:58 +0530

    added cloudnary environment

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,15 +5,21 @@ import { HttpModule } from '@angular/http';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { CondidatesService } from './services/condidates.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CandidateProfileService } from './services';
+
+
+import { CloudinaryModule, CloudinaryConfiguration } from '@cloudinary/angular-5.x';
+import { Cloudinary } from 'cloudinary-core';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    CloudinaryModule.forRoot({Cloudinary}, { cloud_name: 'neta-dev' } as CloudinaryConfiguration)
   ],
   declarations: [NotFoundComponent],
-  providers:[CondidatesService],
+  providers:[CondidatesService,CandidateProfileService],
   exports:[CommonModule]
 })
 export class SharedModule { }
